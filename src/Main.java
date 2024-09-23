3b916f98164931bf4005a25ba6d11f6079e1b46a
Archan-Dhakal <archandhakal1@gmail.com>
2024-09-23 12:24:24 -0400

    u[dated

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double priceOfAPurchase = 10.0;
+        double salesTax = 0.05 * priceOfAPurchase;
+        System.out.println("The price of the purchase is "+ priceOfAPurchase + " and the sales tax is "+ salesTax);
+    }
+}
